cf7cea3adf3d8f7096a9e963f6b9a0cd6b7300e9
NickAloe <mateo.c.atx@gmail.com>
2023-02-28 20:00:03 -0600

    finishing touches

---
+++ PA1.1.cpp
@@ -0,0 +1,36 @@
+//**********************************
+// PA         : 1
+// Q          : 1
+// Programmer : Mateo Cordeiro
+// Status     : Completed
+// Date       : 02/28/23
+//
+//**********************************
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<time.h>
+
+using namespace std;
+
+int main() 
+{
+    int processID = 1;
+    double arrivalTime = 0;
+
+    srand((unsigned)time(NULL));
+    
+    for (int i = 0; i < 1000; i++)
+    {
+        double tempArrivalTime;
+        tempArrivalTime = (-1.0/2.0) * (log((float)rand() / RAND_MAX));
+        arrivalTime += tempArrivalTime;
+
+        cout << left << setw(5) << processID 
+             << left << setw(8) << setprecision(6) << arrivalTime 
+             << left << setw(5) << tempArrivalTime 
+             << "\n";
+        processID++;
+    }
+}
